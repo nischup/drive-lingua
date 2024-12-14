2a9e09c2c2b96d2f52cab89a38f462a6fabaa4aa
nischup <ahsabbir23@gmail.com>
2024-12-14 18:08:12 +0600

    audio added

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: './tab4.page.html',
   styleUrls: ['./tab4.page.scss'],
+    animations: [
+    trigger('profileImageAnimation', [
+      state('initial', style({ opacity: 0, transform: 'scale(0.8)' })),
+      state('final', style({ opacity: 1, transform: 'scale(1)' })),
+      transition('initial => final', animate('1.5s ease-in-out'))
+    ])]
 })
 export class Tab4Page implements OnInit {
 
+ animationState = 'initial';
+ 
   constructor() { }
 
-  ngOnInit() {
+   ngOnInit() {
+    setTimeout(() => {
+      this.animationState = 'final';
+    }, 100); // Trigger animation after component loads
   }
 
   openNotification() {
